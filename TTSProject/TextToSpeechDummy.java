405dc2a28cf9f15b3603a4b5815392abaa1f44b5
Mx1te <peukertmalte@gmail.com>
2025-05-11 15:14:37 +0200

    SpringBoot

--- TTSProject/TextToSpeechDummy.java
+++ TTSProject/TextToSpeechDummy.java
@@ -10,10 +10,10 @@ public class TextToSpeechDummy {
     public static void main(String[] args) {
 
         System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory"); //med quality free Voice
-        // System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_time_awb.AlanVoiceDirectory"); //high quality limited Voice
+        
 
         Voice voice = VoiceManager.getInstance().getVoice("kevin16");
-        // Voice voice = VoiceManager.getInstance().getVoice("alan");
+    
 
 
         // Print all available voices
@@ -26,16 +26,13 @@ public class TextToSpeechDummy {
         if (voice != null) {
             voice.allocate();
 
-            System.out.println("Voice Rate: " + voice.getRate());
-            System.out.println("Voice Pitch: " + voice.getPitch());
-            System.out.println("Voice Volume: " + voice.getVolume());
 
             AudioPlayer aplayer = new SingleFileAudioPlayer("E:\\ProgrammingProjects\\TTS-DHSN-Task\\wavedump\\testwav", javax.sound.sampled.AudioFileFormat.Type.WAVE);
 
             voice.setAudioPlayer(aplayer);
 
             boolean status = voice.speak("Hello, this is a test of the FreeTTS text-to-speech system.");
-            // boolean status = voice.speak("The time is now " + java.time.LocalTime.now() + "."); // Alan limited to time anouncement
+            
 
             System.out.println("Status: " + status);
 
