c4112c0091bc45bb8e59318e81778ef5cc9470d9
Kai Kühl <kuehl.a.kai@gmail.com>
2025-05-19 08:37:49 +0200

    functionality given! website and file in /wavedump directory. output in .wav. .mp3 needs jslayers implementaion

--- src/main/java/TTSProject/TTSController.java
+++ src/main/java/TTSProject/TTSController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+//import java.net.http.HttpHeaders;
 
 @RestController
 public class TTSController {
@@ -29,6 +30,8 @@ public class TTSController {
             }
 
             // FreeTTS konfigurieren
+            System.setProperty("freetts.voices",
+            "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
             VoiceManager vm = VoiceManager.getInstance();
             Voice voice = vm.getVoice("kevin16");
             if (voice == null) return ResponseEntity.status(500).body("Stimme 'kevin16' nicht gefunden.");
